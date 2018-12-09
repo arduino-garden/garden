6de36bc682c60633eb19c3ecedd6da8a8f8a4e3b
Didi Milikina <didimilikina8@gmail.com>
2018-12-09 22:37:51 +0200

    Added new files and deleted unused

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TimeChart from './TimeChart';
+import VoronoiLineChart from './VoronoiLineChart'; 
+import ZoomableChart from './ZoomableChart';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TimeChart />, document.getElementById('root'));
+//ReactDOM.render(<VoronoiLineChart />, document.getElementById('root'));
+//ReactDOM.render(<ZoomableChart />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
